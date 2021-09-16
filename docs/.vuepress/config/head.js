16add006d293619e5f4a1f1b8edf76d39fefaf42
Dragon1573 <Legend_1949@126.com>
2021-09-16 10:13:50 +0800

    Vdoing: 执行 SEO 优化
    - 修改站点默认语言 {en-US => zh-CN}
    - 添加站点默认编码格式
    - 添加移动端缩放优化

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -4,4 +4,11 @@ module.exports = [
   ['meta', { name: 'keywords', content: 'Plotly,Python' }],
   // 移动浏览器的主题背景色
   ['meta', { name: 'theme-color', content: '#11a8cd' }],
+  // 站点默认文件编码格式
+  ['meta', { charset: 'UTF-8' }],
+  // 移动端缩放优化
+  ['meta', {
+    name: 'viewport',
+    content: 'width=device-width, initial-scale=1'
+  }]
 ]
