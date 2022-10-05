6a0b84b6b892c0aca1f570b2d4b527ef5f955c84
Dragon1573 <49941141+Dragon1573@users.noreply.github.com>
2022-10-05 12:27:08 +0800

    更新 SEO 关键词
    - 移除 Plotly 相关内容
    
    Signed-off-by: Dragon1573 <49941141+Dragon1573@users.noreply.github.com>

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -1,7 +1,7 @@
 module.exports = [
   ['link', { rel: 'icon', href: '/favicon.png' }],
   // 首页关键词，方便搜索引擎 SEO 优化
-  ['meta', { name: 'keywords', content: 'Plotly,Python' }],
+  ['meta', { name: 'keywords', content: 'Python' }],
   // 移动浏览器的主题背景色
   ['meta', { name: 'theme-color', content: '#11a8cd' }],
   // 站点默认文件编码格式
