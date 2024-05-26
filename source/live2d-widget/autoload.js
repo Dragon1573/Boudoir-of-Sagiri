f0dfc7b81b190033a20f49599495743aff2965ae
是蓝兔哟～ <49941141+Dragon1573@users.noreply.github.com>
2024-05-26 09:15:02 +0800

    [新功能] Live2D: 切换默认模型 (#8)
    - 修复必须存在多个模型的错误
    - 切换 Live2D 模型后端来源
    - 调整 Live2D 面板可选按钮
    - 添加 `yarn-upgrade-all` 用于批量升级 Yarnpkg 依赖项
    
    Signed-off-by: Dragon1573 <49941141+Dragon1573@users.noreply.github.com>

--- source/live2d-widget/autoload.js
+++ source/live2d-widget/autoload.js
@@ -35,9 +35,9 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
+			cdnPath: "/live2d_api/",
 			// tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
-			tools: ["hitokoto", "switch-model", "switch-texture", "photo", "info", "quit"]
+			tools: ["hitokoto", "photo", "info", "quit"]
 		});
 	});
 }
